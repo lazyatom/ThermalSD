7233995e3841faf5f92ceb0970344cabc601a8ce
James Adam <james@lazyatom.com>
2012-01-28 22:35:48 +0000

    first commit

---
+++ ThermalSD.cpp
@@ -0,0 +1,13 @@
+#include "ThermalSD.h"
+
+void ThermalSD::printBitmap(int w, int h, File file) {
+  if (w > 384) return; // maximum width of the printer
+  for (int rowStart=0; rowStart < h; rowStart += 256) {
+    int chunkHeight = ((h - rowStart) > 255) ? 255 : (h - rowStart);
+    writeBytes(18, 42);
+    writeBytes(h, w/8);
+    for (int i=0; i<((w/8)*chunkHeight); i++) {
+      PRINTER_PRINT((uint8_t)file.read());
+    }
+  }
+};
---
+++ ThermalSD.h
@@ -0,0 +1,16 @@
+#ifndef Thermal_SD_h
+#define Thermal_SD_h
+
+#include <Thermal.h>
+#include <SD.h>
+
+class ThermalSD : public Thermal {
+  public:
+    ThermalSD(int RX_Pin, int TX_Pin) : Thermal(RX_Pin, TX_Pin) {
+      _RX_Pin = RX_Pin;
+      _TX_Pin = TX_Pin;
+    };
+
+    void printBitmap(int w, int h, File file);
+};
+#endif
